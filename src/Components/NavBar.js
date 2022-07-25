e67d7708121c53396039c7a304d5abf4e9815014
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-07-25 00:05:36 -0400

    "navbar"

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
+import '../Components/NavBar.css'
 
 export default function NavBar(){
     return (
-        <nav className="Navbar">
+        <nav className="navbar">
         <h1>Budget App</h1>
-            <button>
+            <button className="budget-button">
             {/* NavBar visible on all pages link to budgets INDEX  and Form for New budget*/}
                 <Link to="/budgets">Kenyetta Budget</Link>
             </button>
-            <button>
+            <button className="new-button">
                 <Link to="/budget/new">New Budget</Link>
             </button>
         </nav>
